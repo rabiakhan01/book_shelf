a9f7eb32468d83b724912bc410d5dce4ae5d619b
rabiakhan01 <rabiakham789@gmail.com>
2024-05-08 16:39:08 +0500

    feat: icons added

---
+++ src/assets/icons/icons.js
@@ -0,0 +1,13 @@
+const icons = {
+    //icons
+    search: require('./search.png'),
+    bookmark: require('./bookmark.png'),
+    facebook: require('./facebook.png'),
+    insta: require('./insta.png'),
+    percentage: require('./insta.png'),
+    review: require('./review.png'),
+    star: require('./star.png'),
+    twitter: require('./twitter.png'),
+    vector: require('./Vector.png'),
+    yellow_star: require('./yellow-star.png'),
+}
