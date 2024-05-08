42096abbdc39c942ffa56bf9693c8668e447faf4
rabiakhan01 <rabiakham789@gmail.com>
2024-05-08 14:19:01 +0500

    feat: images exported

---
+++ src/assets/images/images.js
@@ -0,0 +1,37 @@
+const images = {
+
+    //books section
+    book_1: require('./book_1.png'),
+    book_2: require('./book_2.png'),
+    book_3: require('./book_3.png'),
+    book_4: require('./book_4.png'),
+    book_5: require('./book_5.png'),
+    book_6: require('./book_6.png'),
+    book_7: require('./book_7.png'),
+    book_8: require('./book_8.png'),
+    book_9: require('./book_9.png'),
+    book_10: require('./book_10.png'),
+    book_11: require('./book_11.png'),
+    book_12: require('./book_12.png'),
+    book_13: require('./book_13.png'),
+    book_14: require('./book_14.png'),
+    book_15: require('./book_15.png'),
+
+    //authors section 
+    author_1: require('./author_1.jpg'),
+    author_2: require('./author_2.jpg'),
+    author_3: require('./author_3.jpg'),
+    author_4: require('./author_4.jpg'),
+    author_5: require('./author_5.jpg'),
+    author_6: require('./author_6.jpg'),
+    author_7: require('./author_7.jpg'),
+
+    //hero section
+    hero_1: require('./hero_1.jpg'),
+
+    //viewers section
+    viewer_1: require('./viewer_1.png'),
+    viewer_2: require('./viewer_2.png'),
+    viewer_4: require('./viewer_4.png'),
+
+}
